3368ae827609d3674d35696936c07940aca895b1
Marina Vorontsova <mvorontsova2010@gmail.com>
2022-12-12 19:55:47 +0100

    Add Task3

--- src/Java_Basic/Homework1Task2.java
+++ src/Java_Basic/Homework1Task2.java
@@ -1,4 +1,25 @@
 package Java_Basic;
 
 public class Homework1Task2 {
+    public static void main(String[] args) {
+        byte bt = 127;
+        short shr1 = 32766;
+        int i1 = 1000000001;
+        long l2 = 1010101;
+        float f1 = 0.1f;
+        double d1 = 102102102.1;
+        char ch1 = 'A';
+        boolean bl1 = false;
+        boolean bl2 = true;
+
+        System.out.println(bt);
+        System.out.println(shr1);
+        System.out.println(i1);
+        System.out.println(l2);
+        System.out.println(f1);
+        System.out.println(d1);
+        System.out.println(ch1);
+        System.out.println(bl1);
+        System.out.println(bl2);
+    }
 }
---
+++ src/Java_Basic/Homework1Task3.java
@@ -0,0 +1,13 @@
+package Java_Basic;
+
+public class Homework1Task3 {
+    public static void main(String[] args) {
+        int a = 987;
+        int b = a/100;
+        int c = a%100/10;
+        int d = a-b*100-c*10;
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
