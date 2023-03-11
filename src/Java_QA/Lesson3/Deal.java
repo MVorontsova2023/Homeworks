e0ce609618f0ca73d55e5bc23431d5fb37ac9995
Marina Vorontsova <mvorontsova2010@gmail.com>
2023-03-11 12:56:50 +0100

    HW_11_03_2023

---
+++ src/Java_QA/Lesson3/Deal.java
@@ -0,0 +1,10 @@
+package Lesson3;
+
+
+public class Deal {
+    public static void main (String[] args) {
+        int cardsForPlayer = 5;
+                int players;
+                //Scanner scanner = new
+    }
+}
